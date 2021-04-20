f0dd8aa33755a619e5f5a101b7d92d8ab9b42152
StingraySA <wvermaak@mweb.co.za>
2021-04-20 16:37:04 +0200

    Appen Plugin

--- media/Plugins.md
+++ media/Plugins.md
@@ -10,6 +10,7 @@ Here we can keep track of the plugins and for which breach they are.
 | 2021 | [123rf.com][P_151] | 8,292,274 | 28f439448b7a8237e62847d4df48b95d42c1fec4 | 123rf.com_member.sql | [2021-123rf_com.py] | [Me :)] |
 | 2021 | [Nitro_PDF][P_152] | 76,856,990 | 50dbf53333ef77ef59cd170be4c33931e613b8d9 | nitrocloud.tsv | [2021-nitropdf_com.py] | [Me :)] |
 | 2021 | Not Confirmed | 35,376 | cf475b17a7ed3fee45d17ccb57763c7a5c99ad9d | 35k_Wish.com_2021_Stranded.txt | [2021-wish_com.py] | [@leonjza] |
+| 2020 | [Appen.com][P_158] | 1,219,865 | 97248cc3aa42dd3a6bd6b80df88a6d26b630c294 | Appen.txt | [2021-appen_com.py] | [Me :)] |
 | 2020 | [Wattpad Breach][P_155] | 269,631,125</br>23,987,479 | dea79791e87043a3f76e4d75f33855c7278b0197 </br>af9ddbc8138b013c80c882cb109991bd689c25d1 | cleaned.csv</br>wattpad_24133700_lines.txt | [2020-wattpad_com.py]<br/>[2020-wattpad_com1.py] | [Me :)] |
 | 2020 | [Tamodo Breach][P_154] | 440,110 | c8a2ee7508fb3bce0a3aab8a2244757b0540f0c7 | 103.205.96.158.affiliate_master_dev.users.txt | [2020-tamodo_com.py] | [Me :)] |
 | 2015 | [Patreon Breach][P_150] | 2,209,954 | a60acd81dd95c51bedfc056e4caeda86b70ed0d0 | patreon.sql | [2015-patreon_com.py] | [@leonjza] |
@@ -23,6 +24,7 @@ Here we can keep track of the plugins and for which breach they are.
 [P_155]: <https://haveibeenpwned.com/PwnedWebsites#Wattpad>
 [P_156]: <https://haveibeenpwned.com/PwnedWebsites#MeetMindful>
 [P_157]: <https://haveibeenpwned.com/PwnedWebsites#Wishbone2020>
+[P_158]: <https://haveibeenpwned.com/PwnedWebsites#Appen>
 
 [Me :)]: <https://github.com/StingraySA>
 [@leonjza]: <https://github.com/leonjza>
@@ -36,3 +38,4 @@ Here we can keep track of the plugins and for which breach they are.
 [2020-wattpad_com1.py]: <../parsers/2020-wattpad_com1.py>
 [2021-meetmindful_com.py]: <../parsers/2021-meetmindful_com.py>
 [2021-wishbone_com.py]: <../parsers/2021-wishbone_com.py>
+[2021-appen_com.py]: <../parsers/2021-appen_com.py>
---
+++ parsers/Plugin-Template.py
@@ -0,0 +1,52 @@
+from parsers import base
+import collections
+
+
+class Parse(base.Parser):
+    """
+        <website>.com 2020 breach data parser
+        Source File SHA-1: 97248cc3aa42dd3a6bd6b80df88a6d26b630c294
+        Good Lines:
+    """
+
+    name = "None"
+    web = "appen.com"
+    year = "2020"
+
+    def row_format(self, r: str) -> tuple:
+        """
+            Header: id | name | email | encrypted_password | reset_password_token | reset_password_sent_at |
+            remember_created_at | sign_in_count | current_sign_in_at | last_sign_in_at | current_sign_in_ip |  
+            last_sign_in_ip | failed_attempts | unlock_token | locked_at | authentication_token | salt |
+            created_at | updated_at | email_verified_at | email_verification_sent_at | email_verification_token | 
+            unverified_email | phone_number | company | email_subscriber | title | roles_updated_at | quick_sign_up | 
+            internal_contributor_created_at | external_contributor_created_at | requestor_created_at | 
+            resend_verification_email_count | identity_id | disabled_at | terms_of_service_accepted_at |
+            current_team_id | api_team_id
+
+           name,website,year,domain,email,password,hash,salt
+
+            :param r:
+            :return:
+        """
+
+        row = r.split('|')
+
+        email = row[2].strip()
+        pw_hash = row[3].strip()
+        salt = row[16].strip()
+        domain = email.split('@')[1] if '@' in email else ''
+
+        return self.name, self.web, int(self.year), domain, email, '', pw_hash, salt
+
+    def process_rows(self) -> collections.Iterable[tuple]:
+        with open(self.source, 'r', encoding='utf-8', errors='ignore') as source:
+            for row in source:
+                if row is None:
+                    continue
+                
+                if len(row.split('|')) != 38:
+                    print("x", end='', flush=True)
+                    continue
+
+                yield self.row_format(row)
